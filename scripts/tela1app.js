78c6796910cff76f81dbbe3854311b07a73735b6
Alankazamm <bruno.alanc61@gmail.com>
2022-12-21 06:14:11 -0300

    interactive hm-page button text and js file created

---
+++ scripts/tela1app.js
@@ -0,0 +1,3 @@
+let btn = document.querySelector('#btn-hm-pg');
+btn.addEventListener("mouseleave", () => btn.innerText = "Let's go")
+btn.addEventListener("mouseenter", () => btn.innerText = "Just Go Ahead")
