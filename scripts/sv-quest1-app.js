746c9d9930cbf6840e697148ab99298334fe98c9
Alankazamm <bruno.alanc61@gmail.com>
2022-12-26 08:41:02 -0300

    qst1 & qst2 js done

--- scripts/sv-quest1-app.js
+++ scripts/sv-quest1-app.js
@@ -1,15 +1,29 @@
 function checaMarcado(inputSelect, inputRadio) {
     // console.log(inputSelect.value !== 'hvnt' && inputRadio !== '');
     let checkRadio = false;
-    inputRadio.forEach(element => {
-        if (element.checked === true) {
-            checkRadio = true;
-        }
-    })
-    console.log(checkRadio)
-    return (checkRadio === true && inputSelect.getAttribute('class') === 'changed');
+    inputRadio.forEach(element => element.checked ? checkRadio = true : checkRadio);
+
+    let checkSelect = false;
+    if (inputSelect.getAttribute('class') === 'changed') checkSelect = true;
+
+    if (checkSelect === false || checkRadio === false) {
+        alert("Please, fill the required fields or click 'Skip'!");
+        checkRadio ? checkRadio : sinalizaInput(document.querySelector('.df-form-section'));
+        checkSelect ? checkSelect : sinalizaInput(inputSelect);
+    } 
+
+   
+    return (checkRadio && checkSelect);
 }
 
 function selectClicked() {
     document.querySelector(`select[name='sv-select-status']`).setAttribute('class', 'changed');
+}
+
+function sinalizaInput(input) {
+   
+    input.classList.add('animacaoUnselected');
+    setTimeout(() => {
+        input.classList.remove('animacaoUnselected');
+    }, 2000);
 }
--- scripts/sv-quest2-app.js
+++ scripts/sv-quest2-app.js
@@ -5,3 +5,31 @@ function checaMarcado() {
     return (totalChecked.length > 0);
 }
 
+
+
+function checaMarcado() {
+    // console.log(inputSelect.value !== 'hvnt' && inputRadio !== '');
+    let totalChecked = document.querySelectorAll(`input[name='sv-cbx-ivst']:checked`);
+    
+
+
+
+
+    if (totalChecked.length <= 0) {
+        alert("Please, fill the required fields or click 'Skip'!");
+      sinalizaInput(document.querySelectorAll('.ipt-wrap'))
+    } 
+
+   
+    return (totalChecked.length > 0 );
+}
+
+function sinalizaInput(input) {
+   
+    input.forEach(el => {
+        el.classList.add('animacaoUnselected');
+        setTimeout(() => {
+            el.classList.remove('animacaoUnselected');
+        }, 2000);
+    });
+}
