274a27a50c8c0dd51d8415eaaeb53fb0dafd0b4f
Alankazamm <bruno.alanc61@gmail.com>
2022-12-26 11:24:52 -0300

    projeto finalizado

--- scripts/input-patterns-app.js
+++ scripts/input-patterns-app.js
@@ -1,30 +1,48 @@
 let txtInptName = document.querySelector("#fname");
-let namePattern = '^[A-Za-z]+\\s[A-Za-z]';
+let namePattern = /^[A-Za-z]+\s[A-Za-z]/;
 
 let txtInptEmail = document.querySelector("#email");
-let emailPattern = "/^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\w+)*(\\.\\w{2,3})+$/";
+let emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})/;
+
+let nbrAge = document.querySelector("#age");
+let agePattern = /([0-9]|[1-9][0-9]|99)/;
 
 
-let nome = "";
-txtInptName.addEventListener('input', () => {
-    if (txtInptName.value.match(emailPattern)){
-        nome = txtInptName.value;
-    } else txtInptName.value = nome;
-    console.log(nome);
-} )
+txtInptName.pattern = namePattern;
+txtInptEmail.pattern = emailPattern;
+nbrAge.pattern = agePattern;
 
-function ValidarInputs(input1) {
-    const mailPattern = "/^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\w+)*(\\.\\w{2,3})+$/";
-    if (input1.value.test(mailPattern)) {
-        return true;
+
+function checaMarcado() {
+    // console.log(inputSelect.value !== 'hvnt' && inputRadio !== '');
+    let checkNome = false;
+    let checkEmail = false;
+    let checkAge = false;
+
+    (namePattern.test(txtInptName.value) && (txtInptName.value.length > 0)) ? checkNome = true : checkNome;
+
+    (emailPattern.test(txtInptEmail.value) && (txtInptEmail.value.length > 0)) ? checkEmail = true : checkEmail;
+
+    (agePattern.test(nbrAge.value) && (nbrAge.value.length > 0)) ? checkAge = true : checkAge;
+
+
+    if (!txtInptName || !checkEmail || !checkAge) {
+        alert("Please, fill correctly the required fields!");
+        checkNome ? checkNome : sinalizaInput(txtInptName);
+        checkEmail ? checkEmail : sinalizaInput(txtInptEmail);
+        checkAge ? checkAge : sinalizaInput(nbrAge);
     }
-    else {
-        const teste = document.createElement('p');
-        teste.setAttribute('id', 'teste');
-        teste.innerText = 'Oi';
-        document.body.append(teste);
-        return false;
-    }
+
+
+    return (checkNome && checkEmail && checkAge);
 }
 
 
+
+function sinalizaInput(input) {
+
+    input.classList.add('animacaoUnselected');
+    setTimeout(() => {
+        input.classList.remove('animacaoUnselected');
+    }, 2000);
+}
