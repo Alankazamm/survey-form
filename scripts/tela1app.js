0b76a83c76d054ce5f5bb38d51f0ed50ca686199
Alankazamm <bruno.alanc61@gmail.com>
2023-01-28 01:19:09 -0300

    pg1 done

--- scripts/tela1app.js
+++
@@ -1,6 +0,0 @@
-let btn = document.querySelector('#btn-hm-pg');
-btn.addEventListener("mouseleave", () => btn.innerText = "Let's go")
-btn.addEventListener("mouseenter", () => btn.innerText = "Just Go Ahead")
-btn.addEventListener( "touchend", () => btn.innerText = "Let's go")
-btn.addEventListener( "touchstart", () => btn.innerText = "Just Go Ahead")
-
