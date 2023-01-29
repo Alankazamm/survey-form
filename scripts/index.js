1c7b7d91c26250be9df1d87a98c8cc1e9f634c43
Alankazamm <bruno.alanc61@gmail.com>
2023-01-29 02:02:45 -0300

    basic logic done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,43 @@
-"use strict";
+const pages = [
+    document.querySelector('.welcome-section'),
+    document.querySelector('.start-section'),
+    document.querySelector('.status-section'),
+    document.querySelector('.invest-section'),
+    document.querySelector('.details-section'),
+    document.querySelector('.contact-section')
+];
+const [welcome, start, statuspage, invest, details, contact] = pages;
+const navigate = (page) => {
+    switch (page) {
+        case 'welcome':
+            contact.style.display = 'none';
+            welcome.style.display = 'flex';
+            start.style.display = 'none';
+            break;
+        case 'start':
+            welcome.style.display = 'none';
+            start.style.display = 'flex';
+            statuspage.style.display = 'none';
+            break;
+        case 'status':
+            start.style.display = 'none';
+            statuspage.style.display = 'flex';
+            invest.style.display = 'none';
+            break;
+        case 'invest':
+            statuspage.style.display = 'none';
+            invest.style.display = 'flex';
+            details.style.display = 'none';
+            break;
+        case 'details':
+            invest.style.display = 'none';
+            details.style.display = 'flex';
+            contact.style.display = 'none';
+            break;
+        case 'contact':
+            details.style.display = 'none';
+            contact.style.display = 'flex';
+            break;
+    }
+};
+export default navigate;
